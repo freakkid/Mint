e2549fe922855fff8d681e0667dede6a197744bd
freakkid <904811062@qq.com>
2017-10-16 00:10:25 +0800

    debug

--- 2017服务计算byGo/hw2/hw/t.go
+++ 2017服务计算byGo/hw2/hw/t.go
@@ -8,7 +8,8 @@ import (
 	"strconv"
 	"math"
 	"io/ioutil"
-	"bytes"
+	"bufio"
+	"io"
 )
 
 type ERROR_TYPE int
@@ -23,8 +24,8 @@ const (
 const  (
 	LACK_ARGS ERROR_TYPE = 1 + iota	// 1
 	LACK_S						// 2
-	INVALID_S_NUM				// 3
-	LACK_E						// 4
+	LACK_E						// 3
+	INVALID_S_NUM				// 4
 	INVALID_E_NUM				// 5
 	INVALID_PAGE_LEN			// 6
 	LACK_F						// 7
@@ -84,9 +85,9 @@ func init_selpg(sp_args *Selpg_args) {	// 初始化参数结构
 // 提供报错信息用法并退出
 func usage_and_exit(error_type ERROR_TYPE, str_msg string) {
 	switch {
-	case error_type >= LACK_ARGS && error_type <= INVALID_E_NUM || error_type == LACK_F || error_type == LACK_DEST:
+	case error_type >= LACK_ARGS && error_type <= LACK_E || error_type == LACK_F || error_type == LACK_DEST:
 		fmt.Fprintf(os.Stderr, ERROR_MSG[error_type], progname)		
-	case error_type == INVALID_PAGE_LEN || error_type == UNKNOWN_OPTION:
+	case error_type >= INVALID_S_NUM && error_type <= INVALID_PAGE_LEN || error_type == UNKNOWN_OPTION:
 		fmt.Fprintf(os.Stderr, ERROR_MSG[error_type], progname, str_msg)
 	default:
 		fmt.Fprintf(os.Stderr, "For debug: error usage error type in usage_and_exit")
@@ -114,7 +115,7 @@ func page_ctr_err(error_type ERROR_TYPE, sa_page int, page_ctr int) {
 
 // 判断字符串是否为数字且数字在合理范围内
 func is_valid_str_number(str_num string, int_num *int) bool {
-	if i, err := strconv.Atoi(str_num); err == nil || (i > 0 && i < math.MaxInt32) {
+	if i, err := strconv.Atoi(str_num); err == nil && (i > 0 && i < math.MaxInt32) {
 		*int_num = i
 		return true
 	}
@@ -133,7 +134,7 @@ func process_args(ac int, av []string, sp_args *Selpg_args) {
 	}
 
 	if !is_valid_str_number(av[1][2:], &sp_args.start_page) {
-		usage_and_exit(INVALID_S_NUM, "")
+		usage_and_exit(INVALID_S_NUM, av[1][2:])
 	}
 
 	// ====================  -e  ===================
@@ -142,7 +143,7 @@ func process_args(ac int, av []string, sp_args *Selpg_args) {
 	}
 
 	if !is_valid_str_number(av[2][2:], &sp_args.end_page) || sp_args.end_page < sp_args.start_page {
-		usage_and_exit(INVALID_E_NUM, "")
+		usage_and_exit(INVALID_E_NUM, av[2][2:])
 	}
 
 	argno := 3
@@ -214,24 +215,23 @@ func process_args(ac int, av []string, sp_args *Selpg_args) {
 	fmt.Printf("page_type = %c\n", sp_args.page_type)
 	fmt.Printf("print_dest = %s\n", sp_args.print_dest)
 	fmt.Printf("in_filename = %s\n", sp_args.in_filename)
-	fmt.Printf("===")
 	
 }
 
 func process_input(sp_args Selpg_args) {
-	// var fin *os.File		// nil
+	var fin_ptr *os.File		// nil
 	var fin *bufio.Reader
 	var fout *bufio.Writer
 	var stdinpipe io.WriteCloser
 	var cmd *exec.Cmd
 
-	fmt.Printf("===")
 	// 从命令行或文件输入
 	if (sp_args.in_filename != EMPTY_STRING) {	// 文件输入
 		f, err := os.Open(sp_args.in_filename)	// 打开文件
 		if err != nil {
 			file_err_exit(NOT_OPEN_INPUT_FILE, sp_args.in_filename)
 		}
+		fin_ptr = f
 		fin = bufio.NewReader(f)
 	} else {			// 键盘输入
 		fin = bufio.NewReader(os.Stdin)
@@ -240,14 +240,14 @@ func process_input(sp_args Selpg_args) {
 	// 输出到打印机或屏幕
 	if sp_args.print_dest != EMPTY_STRING {
 		var dest_flag string = fmt.Sprintf("-d%s", sp_args.print_dest)
-		cmd := exec.Command("lp", dest_flag)
+		cmd = exec.Command("lp", dest_flag)
 		stdin, err := cmd.StdinPipe()
 		if err != nil {
 			panic(err)
 		}
 		stdinpipe = stdin
 		fout = bufio.NewWriter(stdin)
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			file_err_exit(NOT_OPEN_PIPE, fmt.Sprintf("lp %s", dest_flag))
 		}
@@ -261,7 +261,7 @@ func process_input(sp_args Selpg_args) {
 		var line_ctr int = 0	// 行数
 		for true {
 			// 读取输入，知道遇到换行符
-			crc, err := fin.ReaderString('\n')
+			crc, err := fin.ReadString('\n')
 			// 处理err信息
 			if err == io.EOF {
 				break
@@ -275,7 +275,7 @@ func process_input(sp_args Selpg_args) {
 			}
 			// 页数在指定范围内则输出
 			if page_ctr >= sp_args.start_page && page_ctr <= sp_args.end_page {
-				_, err: fout.Write([]byte(crc))
+				_, err := fout.Write([]byte(crc))
 				if err != nil {
 					panic(err)
 				}
@@ -294,8 +294,8 @@ func process_input(sp_args Selpg_args) {
 				page_ctr++
 			}
 			// 页数在指定范围内则输出
-			if page_ctr >= sa.start_page && page_ctr <= end_page {
-				err := fout.WriteByte(input)
+			if page_ctr >= sp_args.start_page && page_ctr <= sp_args.end_page {
+				err := fout.WriteByte(input_byte)
 				if err != nil {
 					panic(err)
 				}
@@ -304,17 +304,19 @@ func process_input(sp_args Selpg_args) {
 		}
 	}
 	if page_ctr < sp_args.start_page {
-		page_ctr_err(sp_args.start_page, page_ctr)
+		page_ctr_err(S_PAGE_GREATER_TOTAL_PAGES, sp_args.start_page, page_ctr)
 	} else if page_ctr < sp_args.end_page {
-		page_ctr_err(sp_args.end_page, page_ctr)
+		page_ctr_err(E_PAGE_GREATER_TOTAL_PAGES, sp_args.end_page, page_ctr)
 	}
 	// 正常EOF 没有出错
 	fout.Flush()
-	fin.Close()
-	if (sp_args.print_dest != EMPTY_STRING) {
+	if sp_args.in_filename != EMPTY_STRING {
+		fin_ptr.Close()
+	}
+	if sp_args.print_dest != EMPTY_STRING {
 		stdinpipe.Close()	// 关闭管道
 	}
-	fmt.Printf(os.Stderr, "%s: done\n", progname)
+	fmt.Fprintf(os.Stderr, "%s: done\n", progname)
 
 }
 
@@ -324,7 +326,6 @@ func main() {
 	progname = os.Args[0][strings.LastIndex(os.Args[0], string(os.PathSeparator)) + 1:]
 	
 	process_args(len(os.Args), os.Args, &sp_args)
-	fmt.Printf("===")
 	process_input(sp_args)
 	os.Exit(0)
 }
