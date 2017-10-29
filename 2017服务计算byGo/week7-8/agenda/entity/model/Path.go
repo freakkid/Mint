2b0d324fc4640570fb2b4299f090917b9e7e7002
freakkid <904811062@qq.com>
2017-10-29 14:59:15 +0800

    finish my part

---
+++ 2017服务计算byGo/week7-8/agenda/entity/model/Path.go
@@ -0,0 +1,7 @@
+package model
+
+var DataDirPath string = "data"
+var UserDataPath string = "data/users.json"
+var MeetingDataPath string = "data/meetings.json"
+var CurUserPath string = "data/curUser.txt"
+var LogPath string = "agenda.log"
